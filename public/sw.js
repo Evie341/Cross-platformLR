dabea5b824fdc4b301d07236b1802dc5382cd682
Nic <Nic@gmail.com>
2024-05-21 22:49:42 +0300

    LR3

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
